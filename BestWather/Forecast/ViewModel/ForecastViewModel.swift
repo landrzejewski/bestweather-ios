84294d88a2dcc29ec9ec9804664bc4faa3a1b414
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-01-15 14:03:36 +0100

    provider

--- BestWather/Forecast/ViewModel/ForecastViewModel.swift
+++ BestWather/Forecast/ViewModel/ForecastViewModel.swift
@@ -11,13 +11,15 @@ import Observation
 final class ForecastViewModel {
 
     private let forecastService: ForecastService
+    private let mapper: DayForecastViewModelMapper
     
     var city = ""
     var currentForecast: DayForecastViewModel?
     var nextDaysForecast: [DayForecastViewModel] = []
     
-    init(forecastService: ForecastService) {
+    init(forecastService: ForecastService, mapper: DayForecastViewModelMapper = DayForecastViewModelMapper()) {
         self.forecastService = forecastService
+        self.mapper = mapper
         refreshWeather()
     }
     
@@ -26,7 +28,10 @@ final class ForecastViewModel {
             guard let weather = await forecastService.getWeather(for: "Warsaw") else {
                 return
             }
-            
+            city = weather.city
+            let forecast = mapper.map(forecast: weather.forecast)
+            currentForecast = forecast.first
+            nextDaysForecast = Array(forecast.dropFirst())
         }
     }
     
