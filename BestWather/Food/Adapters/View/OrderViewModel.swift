a472ddae4b25fba884f9a23cc16602acf633645a
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-01-17 08:30:34 +0100

    animations

--- BestWather/Food/Adapters/View/OrderViewModel.swift
+++ BestWather/Food/Adapters/View/OrderViewModel.swift
@@ -12,7 +12,6 @@ final class OrderViewModel {
     
     var orderEntries = [
         OrderEntryViewModel(id: 1, name: "Mega burger", price: "40 zł"),
-        OrderEntryViewModel(id: 2, name: "Asparagus steak", price: "20 zł")
     ]
     var totalPrice = "60 zł"
     
