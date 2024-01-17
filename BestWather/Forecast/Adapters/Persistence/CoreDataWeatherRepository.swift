b0efab6f8e827a5f9cd9bbc8e9a6886c159d16f2
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-01-17 14:10:20 +0100

    animations

--- BestWather/Forecast/Adapters/Persistence/CoreDataWeatherRepository.swift
+++ BestWather/Forecast/Adapters/Persistence/CoreDataWeatherRepository.swift
@@ -17,15 +17,45 @@ final class CoreDataWeatherRepository: WeatherRepository {
     }
    
     func save(weather: Weather) throws {
-        let team = NSEntityDescription.insertNewObject(forEntityName: "ForecastEntity", into: persistence.context()) as! ForecastEntity
+        for forecast in weather.forecast {
+            let forecastEntity = NSEntityDescription.insertNewObject(forEntityName: "ForecastEntity", into: persistence.context()) as! ForecastEntity
+            forecastEntity.date = forecast.date
+            forecastEntity.conditions = forecast.description
+            forecastEntity.temperature = forecast.temperature
+            forecastEntity.pressure = forecast.pressure
+            forecastEntity.icon = forecast.icon
+            forecastEntity.city = weather.city
+            try persistence.context().save()
+        }
     }
     
     func deleteAll() throws {
-        
+        let fetchRequest = ForecastEntity.fetchRequest()
+        fetchRequest.includesPropertyValues = false // get only ids
+        let entities = try persistence.context().fetch(fetchRequest)
+        for entity in entities {
+            persistence.context().delete(entity)
+        }
+        try persistence.context().save()
     }
     
     func get(by city: String, callback: @escaping (Result<Weather, WeatherRepositoryError>) -> ()) {
-        
+        let fetchRequest: NSFetchRequest<ForecastEntity> = ForecastEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "%K = %@", #keyPath(ForecastEntity.city), city)
+        do {
+            let entities = try persistence.context().fetch(fetchRequest)
+            if entities.isEmpty {
+                return callback(.failure(.operationFailed))
+            }
+            let weather = Weather(city: city, forecast: entities.map(map(forecastEntity:)))
+            callback(.success(weather))
+        } catch {
+            callback(.failure(.operationFailed))
+        }
+    }
+    
+    private func map(forecastEntity: ForecastEntity) -> DayForecast {
+        DayForecast(id: UUID(), date: forecastEntity.date!, description: forecastEntity.conditions!, temperature: forecastEntity.temperature, pressure: forecastEntity.pressure, icon: forecastEntity.icon!)
     }
     
 }
